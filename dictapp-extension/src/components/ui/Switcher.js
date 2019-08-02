95da71d43b33f6bc1e303322dd526ce40d52707e
Christofer Flores <cristofer.flort@gmail.com>
2019-08-01 20:35:10 -0500

    Add the flip functionality to the switcher button, pure frontend, still some things to clean up

--- dictapp-extension/src/components/ui/Switcher.js
+++ dictapp-extension/src/components/ui/Switcher.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Switcher(props) {
+
+  const [ enToEs, setLang ] = useState(true)
 
   return (
     <div className="switcher-button-container has-text-centered">
       <div className="columns is-flex is-centered is-vcentered">
         <div className="column is-5">
-          <button className="button is-normal is-fullwidth">Spanish &nbsp;&nbsp;<span className="icon-repeat"></span>&nbsp;&nbsp; English</button>
+          <button className={`button is-normal is-fullwidth flip-button ${enToEs ? '' : 'es'}`} onClick={(e) => { e.preventDefault(); setLang(!enToEs) } }>
+            <div className={`front-button ${ enToEs ? 'active': ''}`}>
+              <p>
+                English &nbsp;&nbsp;<span className="icon-repeat"></span>&nbsp;&nbsp; Spanish
+              </p>
+            </div>
+            <div className={`back-button ${ !enToEs ? 'active': ''}`}>
+              <p>
+                Spanish &nbsp;&nbsp;<span className="icon-repeat"></span>&nbsp;&nbsp; English
+              </p>
+            </div>
+          </button>
         </div>
       </div>
     </div>
